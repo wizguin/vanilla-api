269213bddd80bdb9e561692c16bff7d7ca04b23f
wizguin <16276645+wizguin@users.noreply.github.com>
2024-08-05 14:55:43 +0100

    Add custom error handling

---
+++ src/errors/Errors.ts
@@ -0,0 +1,28 @@
+import { buildError } from '../response/Response'
+
+class ErrorCode extends Error {
+
+    code: number
+    response: string
+
+    constructor(message: string, code: number) {
+        super(message)
+
+        this.code = code
+        this.response = buildError(code)
+    }
+
+}
+
+function createError(message: string, code: number) {
+    return class extends ErrorCode {
+
+        constructor() {
+            super(message, code)
+        }
+
+    }
+}
+
+export const UserNotFoundError = createError('User not found', 100)
+export const IncorrectPasswordError = createError('Incorrect password', 101)
