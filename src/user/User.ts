020966c9d00f955dcab4d419ad548174bae7a80c
wizguin <16276645+wizguin@users.noreply.github.com>
2024-08-06 15:59:40 +0100

    Add account creation functions to user

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,7 +1,18 @@
-import { UserNotFoundError } from '../errors/Errors'
+import { InvalidUsernameError, UserNotFoundError } from '../errors/Errors'
 import Database from '../database/Database'
 
+import { hash } from 'bcrypt'
 import type { User } from '@prisma/client'
+
+export interface CreateUser {
+    username: string,
+    email: string,
+    password: string,
+    color: number
+}
+
+// Letters, numbers, spaces, at least one letter
+const validNameRegex = /^(?=.*[a-zA-Z])[a-zA-Z0-9 ]*$/
 
 export async function getUserByUsername(username: string) {
     if (!username) {
@@ -31,6 +42,32 @@ async function getUser(where: Record<string, number | string>) {
     return user
 }
 
+export async function createUser(data: CreateUser) {
+    const passwordHash = await hash(data.password, 10)
+
+    const user = await Database.user.create({
+        data: {
+            username: data.username,
+            email: data.email,
+            password: passwordHash,
+            color: data.color
+        }
+    })
+
+    await Database.inventory.create({
+        data: {
+            userId: user.id,
+            itemId: data.color
+        }
+    })
+
+    await Database.playerRoom.create({
+        data: {
+            userId: user.id
+        }
+    })
+}
+
 export function getCrumb(user: User) {
     const x = 0
     const y = 0
@@ -54,4 +91,50 @@ export function getCrumb(user: User) {
         frame,
         member
     ].join('|')
+}
+
+export async function validateName(name: string) {
+    name = name.trim().replace(/\s+/g, ' ')
+
+    if (name.length < 4 || name.length > 12) {
+        throw new InvalidUsernameError()
+    }
+
+    if (name.toLowerCase().startsWith('penguin')) {
+        throw new InvalidUsernameError()
+    }
+
+    if (!validNameRegex.test(name)) {
+        throw new InvalidUsernameError()
+    }
+
+    name = toTitleCase(name)
+
+    const user = await Database.user.findFirst({
+        where: {
+            username: name
+        }
+    })
+
+    if (user !== null) {
+        throw new InvalidUsernameError()
+    }
+
+    return name
+}
+
+export function validateColor(color: string) {
+    const parsed = parseInt(color)
+
+    if (!isNaN(parsed) && parsed >= 1 && parsed <= 12) {
+        return parsed
+    }
+
+    return 1
+}
+
+function toTitleCase(str: string) {
+    return str.replace(/\w\S*/g, (word: string) => (
+        word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
+    ))
 }
